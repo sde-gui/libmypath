sde-gui/libmypath

commit b2423c35da81e74016967876d7b4ea623729b3a2

Fix silly string-handling error in get_from_procfs_linux(), get_from_procfs_freebsd(), get_from_procfs_netbsd()

mypath.c
@@ -28,6 +28,18 @@ static void strcat_separator(char * str, char separator)
 
 /*****************************************************************************/
 
+static int is_int(const char * str)
+{
+	while (*str) {
+		if (*str <= '0' || *str >= '9')
+			return 0;
+		str++;
+	}
+	return 1;
+}
+
+/*****************************************************************************/
+
 static char * getcwd_alloc(void)
 {
 	char * buf;
@@ -131,7 +143,7 @@ static char * get_from_procfs_linux(void)
 
 	buf[BUF_SIZE-1] = 0;
 
-	if (buf[0] <= '0' || buf[9] > '9')
+	if (!is_int(buf))
 		goto end;
 
 	if (atol(buf) != getpid())
@@ -160,7 +172,7 @@ static char * get_from_procfs_freebsd(void)
 
 	buf[BUF_SIZE-1] = 0;
 
-	if (buf[0] <= '0' || buf[9] > '9')
+	if (!is_int(buf))
 		goto end;
 
 	if (atol(buf) != getpid())
@@ -197,7 +209,7 @@ static char * get_from_procfs_netbsd(void)
 
 	buf[BUF_SIZE-1] = 0;
 
-	if (buf[0] <= '0' || buf[9] > '9')
+	if (!is_int(buf))
 		goto end;
 
 	if (atol(buf) != getpid())

commit 1bed5999b596751b908f58dd1f6da22305933fed

Fix another stupid error introduced in b2423c35da81e74016967876d7b4ea623729b3a2

mypath.c
@@ -47,7 +47,7 @@ static void strcat_separator(char * str, char separator)
 static int is_int(const char * str)
 {
 	while (*str) {
-		if (*str <= '0' || *str >= '9')
+		if (*str < '0' || *str > '9')
 			return 0;
 		str++;
 	}